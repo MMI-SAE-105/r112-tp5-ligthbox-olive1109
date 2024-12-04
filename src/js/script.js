817d4b209d3503910fb8f94f15dac8c9da4ffc3d
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-12-04 11:09:08 +0100

    js plus transition

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // *** LIGHTBOX ***
+const lightbox = document.getElementById('lightbox');
+    const images = document.querySelectorAll('ul img');
+
+    images.forEach(img => {
+      img.addEventListener('click', () => {
+        const fullImgSrc = img.dataset.fullImg;
+        lightbox.querySelector('img').src = fullImgSrc;
+        lightbox.showModal();
+      });
+    });
+
+    lightbox.addEventListener('click', () => {
+      lightbox.close();
+    });
